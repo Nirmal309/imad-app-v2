2a4d1eae672765f714c3de8399382b129b25afcb
Nirmal309 <13ce100@charusat.edu.in>
2017-02-23 15:17:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,122 +1,19 @@
-var express = require('express');
-var morgan = require('morgan');
-var path = require('path');
+var button = document.getElementById('counter');
 
-var app = express();
-app.use(morgan('combined'));
-
-
-var articles={
-    'article-one':{
-      title:'Article one | Tony Stark',
-      heading:'Article one',
-      content:`
-                    <p>
-                        This is a content one. This is a content one. This is a content one. This is a content one. This is a content one. This is a content one. This is a content one. This is a content one. This is a content one. This is a content one. This is a content one. This is a content one. This is a content one.
-                    </p>
-                    <p>
-                        This is a content one. This is a content one. This is a content one. This is a content one. This is a content one. This is a content one. This is a content one. This is a content one. This is a content one. This is a content one. This is a content one. This is a content one. This is a content one.
-                    </p>
+button.onclick = function(){
+    var request = new XMLHttpRequest();
+    
+    request.onreadystatechange = function(){
+        if (request.readyState === XMLHttpRequest.DONE){
+            if (request.status === 200){
+                var coutner = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
                 
-                    <p>
-                        This is a content one. This is a content one. This is a content one. This is a content one. This is a content one. This is a content one. This is a content one. This is a content one. This is a content one. This is a content one. This is a content one. This is a content one. This is a content one.
-                    </p>`
-    },
-    'article-two':{
-        title:'Article Two | Tony Stark',
-      heading:'Article Two',
-      content:`
-                    <p>
-                        This is a content two. This is a content one. This is a content one. This is a content one. This is a content one. This is a content one. This is a content one. This is a content one. This is a content one. This is a content one. This is a content one. This is a content one. This is a content one.
-                    </p>
-                    <p>
-                        This is a content one. This is a content one. This is a content one. This is a content one. This is a content one. This is a content one. This is a content one. This is a content one. This is a content one. This is a content one. This is a content one. This is a content one. This is a content one.
-                    </p>
-                
-                    <p>
-                        This is a content one. This is a content one. This is a content one. This is a content one. This is a content one. This is a content one. This is a content one. This is a content one. This is a content one. This is a content one. This is a content one. This is a content one. This is a content one.
-                    </p>`
-    },
-    'article-three':{
-        title:'Article Three | Tony Stark',
-      heading:'Article Three',
-      content:`
-                    <p>
-                        This is a content three. This is a content one. This is a content one. This is a content one. This is a content one. This is a content one. This is a content one. This is a content one. This is a content one. This is a content one. This is a content one. This is a content one. This is a content one.
-                    </p>
-                    <p>
-                        This is a content one. This is a content one. This is a content one. This is a content one. This is a content one. This is a content one. This is a content one. This is a content one. This is a content one. This is a content one. This is a content one. This is a content one. This is a content one.
-                    </p>
-                
-                    <p>
-                        This is a content one. This is a content one. This is a content one. This is a content one. This is a content one. This is a content one. This is a content one. This is a content one. This is a content one. This is a content one. This is a content one. This is a content one. This is a content one.
-                    </p>`
-    },
+            }
+        }
+    };
+    
+    request.open('GET','http://nirmal309.imad.hasura-app.io/counter',true);
+    request.send(null);
 };
-
-function createTemplate(data){
-        var title=data.title;
-        var heading=data.heading;
-        var content=data.content;
-        var htmlTemplate=`
-            <html>
-                <head>
-                    <title>
-                        ${title}
-                    </title>
-                    <meta name="viewport" content="width=device-width,initial-scale=1"/>
-                    <link href="/ui/style.css" rel="stylesheet"/>
-                </head>
-                <body>
-                    <div class="container">
-                        <div>
-                            <a href="/">Home</a>
-                        </div>
-                        <hr/>
-                        <h3>
-                            ${heading}
-                        </h3>
-                        <div>
-                           ${content}
-                        </div>
-                    </div>
-                </body>
-            </html>
-        `;
-        return htmlTemplate;
-}
-
-
-
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-});
-
-var counter = 0;
-app.get('/counter', function(req, res){
-    counter=counter+1;
-    res.send(counter.toString());
-});
-
-
-app.get('/:articleName',function(req, res){
-    var articleName=req.params.articleName;
-    res.send(createTemplate(articles[articleName]));
-}
-);
-
-
-
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
-});
-
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
-});
-
-
-var port = 8080; // Use 8080 for local development because you might already have apache running on 80
-app.listen(8080, function () {
-  console.log(`IMAD course app listening on port ${port}!`);
-});
