3310dd82ae479f42fef80ca6500cd68a00e46385
Nirmal309 <13ce100@charusat.edu.in>
2017-02-23 14:24:33 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,9 @@
-console.log('Loaded!');
+// Counter button
+var counter=0;
+var button=document.getElementById('counter');
+
+button.onClick = function(){
+   counter=counter+1;
+   var span=document.getElementById('count');
+   span.innerHtml=counter.toString();
+} ;
